460de44aae1138bb23451319b7a7ff071a5017fb
josephguluzada <yquluzade98@gmail.com>
2024-05-13 17:31:09 +0400

    added files

---
+++ PB201AlgorithmsLoops/PB201AlgorithmsLoops/Program.cs
@@ -0,0 +1,189 @@
+﻿namespace PB201AlgorithmsLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int grade = 0;
+            //if(grade < 0 || grade > 100)
+            //{
+            //    Console.WriteLine("Bele bir grade yoxdur!");
+            //}
+            //else if(grade <65)
+            //{
+            //    Console.WriteLine("Kesildin");
+            //}
+            //else if(grade >= 65 && grade < 85)
+            //{
+            //    Console.WriteLine("Adi Diplom");
+            //}
+            //else if(grade >= 85 && grade < 95)
+            //{
+            //    Console.WriteLine("Honour Diplom");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("High Honour Diplom");
+            //}
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0) // i % 15 == 0
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int i = 1;
+
+            //while (i <= 100)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //int weekDay = 3;
+
+            //if(weekDay == 1)
+            //{
+            //    Console.WriteLine("Monday");
+            //}else if(weekDay == 2)
+            //{
+            //    Console.WriteLine("Tue");
+            //}else if(weekDay == 3)
+            //{
+            //    Console.WriteLine("Wed");
+            //}else if(weekDay == 4)
+            //{
+            //    Console.WriteLine("Thur");
+            //}else if(weekDay == 5)
+            //{
+            //    Console.WriteLine("Fri");
+            //}else if(weekDay == 6)
+            //{
+            //    Console.WriteLine("Sat");
+            //}else if(weekDay == 7)
+            //{
+            //    Console.WriteLine("Sun");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There is no weekday like that");
+            //}
+
+
+            //int weekDay = 3;
+            //switch (weekDay)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Mon");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tue");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wed");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("There is no weekday like that");
+            //        break;
+            //}
+
+            //int i = 0;
+            //int sum = 0;
+            //while (i <= 100)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine("Sum is " + sum );
+
+
+            //int sum = 0;
+            //for(int i = 0; i <= 100; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+            //string name = string.Empty;
+            //int counter = 1;
+            //do
+            //{
+            //    if(counter == 1)
+            //    {
+            //        Console.WriteLine("Enter your name");
+            //        name = Console.ReadLine();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Name length must be min 3 character");
+            //        name = Console.ReadLine();
+            //    }
+            //    counter++;
+            //} while (name.Length < 3);
+
+            //string name = string.Empty;
+            //bool flag = false;
+            //do
+            //{
+            //    if(flag == false)
+            //    {
+            //        Console.WriteLine("Enter your name");
+            //        name = Console.ReadLine();
+            //        flag = true;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Name length must be min 3 character");
+            //        name = Console.ReadLine();
+            //    }
+
+            //} while (name.Length < 3);
+
+            //int number = 0;
+            //int i = 1;
+            //int temp = 1;
+
+            //while (i <= number)
+            //{
+            //    temp *= i;
+            //    i++;
+            //}
+
+            //Console.WriteLine($"{number}! Factorial is: {temp}");
+
+            //for (int i = 1; i <= 8; i++)
+            //{
+            //   if(i == 2 || i == 3)
+            //   {
+            //        continue;
+            //   }
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i <= 8; i++)
+            //{
+            //    if (i == 5)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+        }
+    }
+}
